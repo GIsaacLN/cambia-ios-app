ddf178769c47ef7b4c7c1adfc0195c23ac5a4ae9
Isaac Lopez <isaac.lopez.1b@gmail.com>
2024-10-07 14:58:09 -0600

    Initial Commit

---
+++ cambia/cambiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cambiaApp.swift
+//  cambia
+//
+//  Created by Gustavo Isaac Lopez Nunez on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct cambiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
