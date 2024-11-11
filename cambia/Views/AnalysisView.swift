cba420ffb1a331457baddc962f1c422cececdb35
yatziripineda <131062655+yatziripineda@users.noreply.github.com>
2024-11-10 19:27:05 -0600

    gráficas
    Se realizaron dos gráficas

--- cambia/Views/AnalysisView.swift
+++ cambia/Views/AnalysisView.swift
@@ -1,66 +1,244 @@
 import SwiftUI
+import Charts
 
 struct AnalysisView: View {
     @EnvironmentObject var metricsViewModel: MetricsViewModel
+    @State var oberlayDataServivios: Bool = false
+    @State var showDistribucionOverlay: Bool = false
+    @State var ShowPrediccióndeInundación: Bool = true
     
     var body: some View {
         NavigationStack {
-            VStack(alignment: .leading, spacing: 20) {
-                Text("Análisis de Riesgo de Inundación")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding(.top)
-                
-                // Predicción de Riesgo de Inundación
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Predicción de Inundación")
-                        .font(.headline)
-                        .foregroundColor(.white.opacity(0.7))
+           // ScrollView{
+                VStack(alignment: .leading, spacing: 20) {
                     
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 12)
-                            .fill(Color.blue.opacity(0.2))
-                            .frame(height: 80)
+                    Text("Análisis de Riesgo de Inundación")
+                        .font(.subheadline)
+                        .fontWeight(.bold)
+                    
+                    // Predicción de Riesgo de Inundación
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Predicción de Inundación")
+                            .font(.caption)
+                            .foregroundColor(.white.opacity(0.7))
                         
-                        HStack {
-                            Image(systemName: metricsViewModel.floodRiskPrediction.contains("Alto") ? "exclamationmark.triangle.fill" : "checkmark.circle.fill")
-                                .font(.system(size: 40))
-                                .foregroundColor(metricsViewModel.floodRiskPrediction.contains("Alto") ? .red : .green)
-                            Spacer()
-                            Text(metricsViewModel.floodRiskPrediction)
-                                .font(.headline)
-                                .fontWeight(.semibold)
-                                .foregroundColor(.white)
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 12)
+                                .fill(Color.blue.opacity(0.2))
+                                .frame(height: 80)
+                            
+                            HStack {
+                                Image(systemName: metricsViewModel.floodRiskPrediction.contains("Alto") ? "exclamationmark.triangle.fill" : "checkmark.circle.fill")
+                                    .font(.system(size: 40))
+                                    .foregroundColor(metricsViewModel.floodRiskPrediction.contains("Alto") ? .red : .green)
+                                Spacer()
+                                Text(metricsViewModel.floodRiskPrediction)
+                                    .font(.headline)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.white)
+                                Button {
+                                    ShowPrediccióndeInundación.toggle()
+                                } label: {
+                                    Image(systemName: "info.circle")
+                                        .foregroundStyle(.teal)
+                                }.padding()
+                                    
+                            }
+                            .padding(.horizontal)
                         }
-                        .padding(.horizontal)
                     }
-                }
-                
-                Divider().background(Color.gray.opacity(0.3))
-                
-                // Información de las métricas
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Detalles del Análisis")
-                        .font(.headline)
-                        .foregroundColor(.white.opacity(0.7))
+                   
+                    if ShowPrediccióndeInundación{
+                        Divider().background(Color.gray.opacity(0.3))
+                        
+                        // Información de las métricas
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("Detalles del Análisis")
+                                .font(.headline)
+                                .foregroundColor(.white.opacity(0.7))
+                            
+                            MetricRow(title: "Densidad Poblacional", value: metricsViewModel.inegiData?.indicators["densidad"].map { "\(Int($0)) Hab/Km²" } ?? "No disponible")
+                            MetricRow(title: "Área Inundada", value: metricsViewModel.inundatedArea.map { "\(String(format: "%.2f", $0)) Km²" } ?? "No disponible")
+                            MetricRow(title: "Precipitación Anual", value: "\(Int(metricsViewModel.annualPrecipitation ?? 0)) mm")
+                            MetricRow(title: "Distancia promedio al Hospital más cercano", value: "\(String(format: "%.2f", metricsViewModel.averageHospitalDistance)) km")
+                            MetricRow(title: "Número de Hospitales en un radio de 10 km", value: "\(metricsViewModel.totalHospitalsInMunicipio)")
+                            MetricRow(title: "Área Total de la Ciudad", value: "\(String(format: "%.2f", metricsViewModel.cityArea ?? 0)) Km²")
+                        }
+                    }
                     
-                    MetricRow(title: "Densidad Poblacional", value: metricsViewModel.inegiData?.indicators["densidad"].map { "\(Int($0)) Hab/Km²" } ?? "No disponible")
-                    MetricRow(title: "Área Inundada", value: metricsViewModel.inundatedArea.map { "\(String(format: "%.2f", $0)) Km²" } ?? "No disponible")
-                    MetricRow(title: "Precipitación Anual", value: "\(Int(metricsViewModel.annualPrecipitation ?? 0)) mm")
-                    MetricRow(title: "Distancia promedio al Hospital más cercano", value: "\(String(format: "%.2f", metricsViewModel.averageHospitalDistance)) km")
-                    MetricRow(title: "Número de Hospitales en un radio de 10 km", value: "\(metricsViewModel.totalHospitalsInMunicipio)")
-                    MetricRow(title: "Área Total de la Ciudad", value: "\(String(format: "%.2f", metricsViewModel.cityArea ?? 0)) Km²")
+                    HStack{
+                        GraficaServiciosBasicos()
+                            .padding()
+                        GraficaDistribucionViviendas()
+                            .padding()
+                    }
+                    .padding()
+                    Spacer()
+                    
                 }
-                
-                Spacer()
-            }
-            .padding()
-            .background(Color.gray5.edgesIgnoringSafeArea(.all))
+                .padding()
+                .background(Color.gray5.edgesIgnoringSafeArea(.all))
+            //}
         }
         .onAppear {
             metricsViewModel.performPrediction()
         }
     }
+    
+    
+    func GraficaServiciosBasicos() -> some View {
+        VStack {
+           Text("Servicios Básicos")
+            .font(.subheadline)
+            .foregroundColor(.secondary)
+            Chart {
+                BarMark(
+                    x: .value("Servicio", "Hospitales"),
+                    y: .value("Cantidad", metricsViewModel.totalHospitalsInMunicipio)
+                )
+                .foregroundStyle(.red)
+                BarMark(
+                    x: .value("Servicio", "Policía"),
+                    y: .value("Cantidad", metricsViewModel.totalPoliceStationsInMunicipio)
+                )
+                .foregroundStyle(.blue)
+                
+                BarMark(
+                    x: .value("Servicio", "Bomberos"),
+                    y: .value("Cantidad", metricsViewModel.totalFireStationsInMunicipio)
+                )
+                .foregroundStyle(.yellow)
+            }
+            .frame(width: 250, height: 250)
+            .onTapGesture {
+                oberlayDataServivios.toggle()
+            }
+            .overlay{
+                if oberlayDataServivios {
+                    VStack (alignment: .leading){
+                        Text("Interpretación")
+                            .font(.headline)
+                            .padding(.bottom, 5)
+                        Text("HOSPITALES")
+                            .font(.caption2)
+                            .foregroundStyle(.white,.opacity(50))
+
+                        Text(serviceDescription(for:"Hospitales"))
+                            .font(.body)
+                            .multilineTextAlignment(.leading)
+                        Divider()
+                        Text("POLICÍA")
+                            .font(.caption2)
+                            .foregroundStyle(.white,.opacity(50))
+                        Text(serviceDescription(for:"Policía"))
+                            .font(.body)
+                            .multilineTextAlignment(.leading)
+                        Divider()
+                        Text("BOMBEROS")
+                            .font(.caption2)
+                            .foregroundStyle(.white,.opacity(50))
+
+                        Text(serviceDescription(for:"Bomberos"))
+                            .font(.body)
+                            .multilineTextAlignment(.leading)
+                        
+                    }
+                    .padding()
+                    .frame(width: 240, height: 300)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .shadow(radius: 5)
+                    .onTapGesture {
+                        oberlayDataServivios.toggle()
+                    }
+                }
+            }
+            
+        }
+        .padding()
+        
+    }
+    
+    
+    func GraficaDistribucionViviendas() -> some View {
+        let viviendaData = [
+            ("Electricidad", metricsViewModel.inegiData?.indicators["viviendasConElectricidad"]),
+                    ("Agua", metricsViewModel.inegiData?.indicators["viviendasConAgua"] ?? 0)
+                ]
+                
+                return VStack {
+                    Text("Distribución de Viviendas")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                    
+                    Chart(viviendaData, id: \.0) { data in
+                        BarMark(
+                            x: .value("Tipo", data.0),
+                            y: .value("Porcentaje", data.1 ?? 0)
+                        )
+                        .foregroundStyle(by: .value("Tipo", data.0))
+                    }
+                    .frame(width: 250, height: 250)
+                    .onTapGesture {
+                        showDistribucionOverlay.toggle()
+                    }
+                    .overlay {
+                        if showDistribucionOverlay {
+                            VStack(alignment: .leading) {
+                                Text("Interpretación:")
+                                    .font(.headline)
+                                    .padding(.bottom, 5)
+                                Text(viviendasDescription())
+                            }
+                            .padding()
+                            .background(Color(.systemGray6))
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .onTapGesture {
+                                showDistribucionOverlay.toggle()
+                            }
+                        }
+                    }
+                }
+                .padding()
+            }
+    
+    private func serviceDescription(for service: String) -> String {
+        guard let population = metricsViewModel.inegiData?.indicators["poblacionTotal"] else {
+            return "Datos de población no disponibles."
+        }
+
+        let serviceCount: Int
+        let description: String
+
+        switch service {
+        case "Hospitales":
+            serviceCount = metricsViewModel.totalHospitalsInMunicipio
+            description = "Un hospital disponible por cada \(Int(population) / max(serviceCount, 1)) habitantes."
+        case "Policía":
+            serviceCount = metricsViewModel.totalPoliceStationsInMunicipio
+            description = "Una estación de policía por cada \(Int(population) / max(serviceCount, 1)) habitantes."
+        case "Bomberos":
+            serviceCount = metricsViewModel.totalFireStationsInMunicipio
+            description = "Una estación por cada \(Int(population) / max(serviceCount, 1)) habitantes"
+        default:
+            description = "Datos no disponibles para este servicio."
+        }
+
+        return description
+    }
+    
+    private func viviendasDescription() -> String {
+        guard let electricidad = metricsViewModel.inegiData?.indicators["viviendasConElectricidad"],
+              let agua = metricsViewModel.inegiData?.indicators["viviendasConAgua"] else {
+            return "Datos de distribución de viviendas no disponibles."
+        }
+        
+        return """
+        En este municipio, el \(String(format: "%.2f", electricidad))% de las viviendas tienen acceso a electricidad y el \(String(format: "%.2f", agua))% tienen acceso a agua potable. 
+        """
+    }
+
 }
 
 // Componentes de UI reutilizables
